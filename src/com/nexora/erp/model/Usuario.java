a834c729b23443586764f2a0578d0defa4f6ff86
luque-07 <adrian.luquehernandez@gmail.com>
2026-03-03 01:24:18 +0100

    Agregar enunciado

--- src/com/nexora/erp/model/Usuario.java
+++ src/com/nexora/erp/model/Usuario.java
@@ -1,5 +1,13 @@
 package com.nexora.erp.model;
 
-public class Usuario {
-
+public abstract class Usuario {
+	
+	//Atributos
+	
+	protected String id;
+	protected String nombre;
+	protected String apellidos;
+	protected String contrasenia;
+	protected String rol;
+	
 }
