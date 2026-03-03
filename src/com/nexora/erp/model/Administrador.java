b632d23e753877130c2efbac54362e49373b06e2
luque-07 <adrian.luquehernandez@gmail.com>
2026-03-03 02:13:00 +0100

    [Fase1] Usuario

--- src/com/nexora/erp/model/Administrador.java
+++ src/com/nexora/erp/model/Administrador.java
@@ -1,5 +1,14 @@
 package com.nexora.erp.model;
 
-public class Administrador {
+public class Administrador extends Usuario{
 
+	public Administrador(String nombre, String email, String contra){
+		super();
+		this.id = "ad";
+		this.nombre = nombre;
+		this.contrasenia = contra;
+		this.email = email;
+		this.rol = "Administrador";
+	}
+	
 }
--- src/com/nexora/erp/model/Usuario.java
+++ src/com/nexora/erp/model/Usuario.java
@@ -6,8 +6,57 @@ public abstract class Usuario {
 	
 	protected String id;
 	protected String nombre;
-	protected String apellidos;
 	protected String contrasenia;
 	protected String rol;
+	protected String email;
 	
-}
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", contrasenia=" + contrasenia + ", rol=" + rol + ", email="
+				+ email + "]";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	}
+	
